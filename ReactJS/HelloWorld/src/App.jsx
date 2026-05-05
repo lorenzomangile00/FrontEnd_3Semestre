1045f992a812942136f32a6924104d06ef503dc8
LORENZO MANGILE FARIA <53101823811@info.local>
2026-05-05 16:45:34 -0300

    Ativdades de react

---
+++ ReactJS/HelloWorld/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Paragrafo from './components/paragrafo/paragrafo';
+import Title from './components/title/title';
+
+function App() {
+  return (
+    <>
+      <Title texto="Bem Vindo, sou Título"/>
+      <Title texto="Eu sou outro titulo"/>
+      <Paragrafo textoParagrafo="Esse é o paragrafo"/>
+    </>
+  );
+};
+
+export default App
+//criar um componente title
